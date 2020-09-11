dfad4222bd0e4f90252b318dd110bffee145a995
steve14c <steve14c@gmail.com>
2020-09-11 05:15:14 -0700

    Draw colored cube and learning pipeline basic

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 
 ## Notes
 
-###Translation Matrices
+###### Transformation
+Translation Matrices
 ```
 1 0 0 X
 0 1 0 Y
@@ -11,7 +12,7 @@
 0 0 0 1
 ```
 
-###Identity Matrices
+Identity Matrices
 ```
 1 0 0 0
 0 1 0 0
@@ -21,7 +22,7 @@
  
 does not do anything, but it's important to know multiply this will give the original matrix
 
-###Scalling Matrices
+Scalling Matrices
 ```
 X 0 0 0
 0 Y 0 0
@@ -29,9 +30,16 @@ X 0 0 0
 0 0 0 1
 ```
 
-###Rotation Matrices
+Rotation Matrices
 ```
 glm::vec3 myRotationAxis( ??, ??, ??);
 glm::rotate( angle_in_degrees, myRotationAxis );
 ```
 it's not clear yet how rotation works behind the scene
+
+##### Pipeline
+      - Vertex -> Fragment
+      - Bind different buffer to draw different object in the main loop, remember to call attribute pointer function again.
+      - Buffer data need to be updated in order to change color in the main loop.
+      - Only the last gl_position assighment will be effective.
+
--- playground/playground.cpp
+++ playground/playground.cpp
@@ -7,9 +7,11 @@
 GLFWwindow* window;
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 
 #include <glm/glm.hpp>
 using namespace glm;
+
 
 #include <common/shader.hpp>
 
@@ -65,44 +67,112 @@ int main( void )
 
 	// 3 vertices for our triangle
 	static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
+		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, // triangle 1 : end
+		1.0f, 1.0f,-1.0f, // triangle 2 : begin
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, // triangle 2 : end
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
 	};
+
+	static const GLfloat tri_vertex_buffer_data[] = {
+		4.0f, 0.0f, 0.0f,
+		6.0f, 0.0f, 0.0f,
+		5.0f, 2.0f, 0.0f,
+	};
+
+	static GLfloat g_color_buffer_data[12 * 3 * 3];
+
+	int r = 0;
+	int g = 1;
+	int b = 2;
+
+	for (int i = 0; i < 12 * 3; ++i)
+	{
+		g_color_buffer_data[i * 3 + 0] = r % 3 * 0.5;
+		g_color_buffer_data[i * 3 + 1] = g % 3 * 0.5;
+		g_color_buffer_data[i * 3 + 2] = b % 3 * 0.5;
+	}
+	
+		
+
 	// this identify our vertex buffer
 	GLuint vertexbuffer;
-
 	// Generate 1 buffer, put the resulting identifier in vertexbuffer
 	glGenBuffers(1, &vertexbuffer);
-
 	// The following commands will talk about our 'vertexbuffer' buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
 	// Give our vertices to OpenGL.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	mat4 Perspec_Projection = perspective(radians(45.0f), float(16) / float(9), 0.1f, 100.0f);
+	
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
+	GLuint tribuffer;
+	glGenBuffers(1, &tribuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, tribuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tri_vertex_buffer_data), tri_vertex_buffer_data, GL_STATIC_DRAW);
+
+	mat4 Perspec_Projection = perspective(radians(45.0f), float(16) / float(9), 0.1f, 100.0f);
 	mat4 Ortho_Projection = ortho(-16.0f, 16.0f, 9.0f, -9.0f, 0.1f, 100.0f);
- 
 	mat4 View = lookAt(
 		vec3(0, 0, 20),
 		vec3(0, 0, 0),
 		vec3(0, 1, 0)
 	);
+	mat4 TranslationMatrix = translate(mat4(), vec3(0.0f));
+	mat4 ScallingMatrix = scale(mat4(), vec3(1.0f));
+	mat4 Model = TranslationMatrix * ScallingMatrix;
+	mat4 cube_mvp = Perspec_Projection * View * Model;
 
-
-	mat4 Model = mat4(1.0f);
-
-	mat4 mvp = Perspec_Projection * View * Model;
+	mat4 tri_mvp = Perspec_Projection * View * Model;
 
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 	
 
+	
+
 	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(programID);
+
+		// Enable depth test
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
 
 
 		glEnableVertexAttribArray(0);
@@ -116,11 +186,39 @@ int main( void )
 			(void*)0
 		);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
 
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &cube_mvp[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		
+
+		glBindBuffer(GL_ARRAY_BUFFER, tribuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &tri_mvp[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	
 
 		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
